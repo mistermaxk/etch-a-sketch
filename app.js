519dc4e2c31adc61966bf16fc9a29a45e6d721ff
Max K <mxkk@pm.me>
2026-02-11 18:50:46 -0500

    Implement minimum viable grid creation

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 // etch-a-sketch
 // max k 2026
 const container = document.querySelector("#container");
+
+function createGrid(size) {
+    for (let i = 0; i < size*size; i++) {
+        let square = document.createElement("div");
+        square.classList.add("grid-square");
+        square.style.flexBasis = `${100/size}%`;
+        square.style.height = `${100/size}%`;
+        container.appendChild(square);
+    }
+}
+
+createGrid(4);
