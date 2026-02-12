50a9732138beb268b35d09a288fe9fd91907a759
Max K <mxkk@pm.me>
2026-02-11 20:40:57 -0500

    Implement change grid logic and UI

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 // etch-a-sketch
 // max k 2026
-const container = document.querySelector("#container");
 
-function createGrid(size) {
+const changeGrid = document.createElement("button");
+changeGrid.textContent = "Change Grid Size";
+changeGrid.addEventListener("click", () => {
+    let gridSize = parseInt(prompt("Enter an integer between 4 and 100:", 16));
+    if (gridSize < 4) gridSize = 4;
+    else if (gridSize > 100) gridSize = 100;
+    const container = document.querySelector("div.container");
+    document.body.removeChild(container);
+    createGrid(gridSize);
+});
+document.body.appendChild(changeGrid);
+
+
+function createGrid(size = 16) {
+    const container = document.createElement("div");
+    container.classList.add("container");
+    document.body.appendChild(container);
+
     for (let i = 0; i < size*size; i++) {
         let square = document.createElement("div");
         square.classList.add("grid-square");
@@ -17,4 +33,4 @@ function hoverEffect(e) {
     e.target.style.backgroundColor = "gray";
 }
 
-createGrid(16);
+createGrid();
